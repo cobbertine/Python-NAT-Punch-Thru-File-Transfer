dd18b8fb02b03b9bd94a4a9103546a46421c8e1c
cobbertine <cobbertine@gmail.com>
2020-11-10 19:05:27 +1100

    Updated create_table.sql based on changes in c2e6ac5

--- server_files/create_table.sql
+++ server_files/create_table.sql
@@ -3,5 +3,6 @@ CREATE TABLE `transfer_details` (
   `uploader_address` varchar(45) NOT NULL DEFAULT '',
   `downloader_address` varchar(45) NOT NULL DEFAULT '',
   `date_added` datetime NOT NULL,
+  `is_ready` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`unique_conn_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
